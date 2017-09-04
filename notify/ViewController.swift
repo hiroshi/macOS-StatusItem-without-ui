2ad6a9fc4fe9d4dc74abbc665e91895c9f20b7b3
hiroshi <hiroshi3110@gmail.com>
2017-09-04 21:13:17 +0900

    Initial Commit

---
+++ notify/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  notify
+//
+//  Created by hiroshi on 2017/09/04.
+//
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
