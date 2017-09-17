0596b35d9bd5950a1c8dfeb64944c91c8b8e514a
hiroshi <hiroshi3110@gmail.com>
2017-09-17 21:59:58 +0900

    statusItem

--- notify/AppDelegate.swift
+++ notify/AppDelegate.swift
@@ -1,15 +1,30 @@
 import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem!
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
         debugPrint("hello")
+        statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+        statusItem.title = "Notify"
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Action 1", action: #selector(AppDelegate.action1(sender:)), keyEquivalent: "e"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.action2(sender:)), keyEquivalent: "k"))
+        statusItem.menu = menu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    @IBAction func action1(sender: AnyObject) {
+        debugPrint("action1")
+    }
 
+    @IBAction func action2(sender: AnyObject) {
+        debugPrint("Quit")
+        NSApplication.shared().terminate(nil)
+
+    }
 }
 
